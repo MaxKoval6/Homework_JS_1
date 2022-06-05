01f165faa8921243a8a385312bb295215a2be99e
MaxKoval6 <mkoval06@gmail.com>
2022-06-05 13:10:59 +0300

    6

--- Task_6.js
+++ Task_6.js
@@ -11,4 +11,18 @@
 // } else {
 //   total = total + input;
 // }
-// alert("Загальна сума чисел дорівнює ${total}");
+// alert(`Загальна сума чисел дорівнює ${total}`);
+
+let total = 0;
+let input;
+
+do {
+  input = prompt("Введіть число");
+  if (isNaN(input)) {
+    alert("Було написано не число, спробуйте ще раз");
+    continue;
+  }
+  total += Number(input);
+} while (input !== null);
+
+alert(total);
